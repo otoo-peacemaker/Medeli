7bc348152d7cb293a300eb5ffbad357745a7a3c5
Peacemaker Otoo <infofeedacc@gmail.com>
2022-11-07 21:12:01 +0000

    backend

---
+++ backend/medeli/src/main/java/com/gads/medeli/MedeliApplication.java
@@ -0,0 +1,13 @@
+package com.gads.medeli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedeliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedeliApplication.class, args);
+    }
+
+}
