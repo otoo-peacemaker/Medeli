43608bdc1ba40e4e557e536caa58869efcf8da5f
Peacemaker Otoo <infofeedacc@gmail.com>
2022-12-16 11:12:12 +0000

    onOptionsItemSelected and notification file

---
+++ frontend/android/app/src/main/java/com/peacecodetech/medeli/ui/main/notification/NotificationFragment.kt
@@ -0,0 +1,34 @@
+package com.peacecodetech.medeli.ui.main.notification
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.peacecodetech.medeli.R
+
+class NotificationFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = NotificationFragment()
+    }
+
+    private lateinit var viewModel: NotificationViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_notification, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(
+            NotificationViewModel::class.java
+        )
+        // TODO: Use the ViewModel
+    }
+
+}
